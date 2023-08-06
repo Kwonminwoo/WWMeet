fb9222d3c20c5ac0d9703b553697112944c5a0e2
KwonMinwoo <qwerty029369@naver.com>
2023-08-06 17:37:48 +0900

    #6 - 홈 뷰, 홈 기능 완성
    홈의 약속 리스트의 어댑터, 약속 도메인, 홈 뷰, 홈의 기능들 완성

--- frontend/app/src/main/java/com/example/wwmeet_android/MainActivity.java
+++ frontend/app/src/main/java/com/example/wwmeet_android/MainActivity.java
@@ -1,16 +1,55 @@
 package com.example.wwmeet_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.wwmeet_android.domain.Appointment;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    RecyclerView recyclerView;
+    Button enterBtn;
+    Button createApmBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        AppointmentBeforeAdapter beforeAdapter = new AppointmentBeforeAdapter();
+        beforeAdapter.setList(new ArrayList<Appointment>()); // 실제 데이터로 바꿔야 함 Todo
+        recyclerView.setAdapter(beforeAdapter);
+
+
+        createApmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 약속 만들기
+            }
+        });
+
+        enterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 약속 입장 하기
+            }
+        });
+
     }
 
+    private void init(){
+        recyclerView = findViewById(R.id.home_recyclerview);
+        enterBtn = findViewById(R.id.home_enter_appoint_btn);
+        createApmBtn = findViewById(R.id.home_create_appoint_btn);
+    }
 
 }
---
+++ frontend/app/src/main/java/com/example/wwmeet_android/domain/Appointment.java
@@ -0,0 +1,72 @@
+package com.example.wwmeet_android.domain;
+
+import java.time.LocalDateTime;
+
+public class Appointment {
+    private Long id;
+    private String code; // 약속 코드
+    private String name; // 약속 이름
+    private int numberPeople; // 약속 인원수
+    private String local; // 약속 장소
+    private LocalDateTime appointDate; // 약속 날짜
+    private LocalDateTime limitDate; // 투표 기한
+
+    public Appointment() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberPeople() {
+        return numberPeople;
+    }
+
+    public void setNumberPeople(int numberPeople) {
+        this.numberPeople = numberPeople;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public LocalDateTime getAppointDate() {
+        return appointDate;
+    }
+
+    public void setAppointDate(LocalDateTime appointDate) {
+        this.appointDate = appointDate;
+    }
+
+    public LocalDateTime getLimitDate() {
+        return limitDate;
+    }
+
+    public void setLimitDate(LocalDateTime limitDate) {
+        this.limitDate = limitDate;
+    }
+}
