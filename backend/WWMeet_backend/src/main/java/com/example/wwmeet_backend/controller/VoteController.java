44bc5f039027a04c026a653bfcc7f46033350083
KwonMinwoo <qwerty029369@naver.com>
2023-09-12 21:54:30 +0900

    #30 - 일정 투표 Controller 코드 작성 완료
    일정 투표를 mapping하는 Controller의 운영 코드 작성 완료

--- backend/WWMeet_backend/src/main/java/com/example/wwmeet_backend/controller/VoteController.java
+++ backend/WWMeet_backend/src/main/java/com/example/wwmeet_backend/controller/VoteController.java
@@ -1,14 +1,45 @@
 package com.example.wwmeet_backend.controller;
 
 
+import com.example.wwmeet_backend.domain.Appointment;
+import com.example.wwmeet_backend.domain.Participant;
+import com.example.wwmeet_backend.domain.PossibleSchedule;
+import com.example.wwmeet_backend.domain.Vote;
+import com.example.wwmeet_backend.dto.VoteRequestDto;
+import com.example.wwmeet_backend.dto.VoteResponseDto;
+import com.example.wwmeet_backend.service.AppointmentService;
+import com.example.wwmeet_backend.service.ParticipantService;
 import com.example.wwmeet_backend.service.VoteService;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class VoteController {
     private final VoteService voteService;
+    private final AppointmentService appointmentService;
+    private final ParticipantService participantService;
+
+    @PostMapping("/api/appointment/vote")
+    public List<VoteResponseDto> voteAppointmentSchedule(@RequestBody List<VoteRequestDto> voteList){
+        Long appointmentId = voteList.get(0).getAppointmentId();
+        Long participantId = voteList.get(0).getParticipantId();
+        Appointment appointment = appointmentService.findAppointmentById(appointmentId);
+        Participant participant = participantService.findParticipantById(participantId);
+        List<VoteResponseDto> voteResponseList = new ArrayList<>();
+        for (VoteRequestDto voteRequest : voteList) {
+            PossibleSchedule possibleSchedule = new PossibleSchedule(null, appointment, voteRequest.getStartTime(), voteRequest.getEndTime());
+            Vote savedVote = voteService.saveVoteSchedule(new Vote(null, participant, possibleSchedule));
+            voteResponseList.add(new VoteResponseDto(savedVote.getId(), participantId, savedVote.getPossibleSchedule().getStartTime(), savedVote.getPossibleSchedule().getEndTime()));
+        }
+
+        return voteResponseList;
+    }
 }
