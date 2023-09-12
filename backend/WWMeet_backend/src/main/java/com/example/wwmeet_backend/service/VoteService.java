c893d9b584c251321b2422784e8bcf7b1f1982bf
KwonMinwoo <qwerty029369@naver.com>
2023-09-12 21:59:00 +0900

    #30 - 일정 투표에 필요한 layer, Controller에서 사용한 메서드 작성
    일정 투표에 사용되는 Service, Repository를 생성, Controller에서 사용되는 메서드를 작성했음.

--- backend/WWMeet_backend/src/main/java/com/example/wwmeet_backend/service/VoteService.java
+++ backend/WWMeet_backend/src/main/java/com/example/wwmeet_backend/service/VoteService.java
@@ -1,12 +1,19 @@
 package com.example.wwmeet_backend.service;
 
 
+import com.example.wwmeet_backend.domain.Vote;
 import com.example.wwmeet_backend.repository.VoteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class VoteService {
     private final VoteRepository voteRepository;
+
+    public Vote saveVoteSchedule(Vote vote) {
+        return null;
+    }
 }
