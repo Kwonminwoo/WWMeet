556f909de75214a3ae745fd0dc1c19b532e365e1
KwonMinwoo <qwerty029369@naver.com>
2023-09-16 12:02:35 +0900

    #36 - 약속 참가자 추가 기능 Cotroller 테스트 코드
    약속에 참가할 사람을 추가하는 기능의 Cotroller 테스트 코드를 작성

--- backend/WWMeet_backend/src/test/java/com/example/wwmeet_backend/controller/AppointmentControllerTest.java
+++ backend/WWMeet_backend/src/test/java/com/example/wwmeet_backend/controller/AppointmentControllerTest.java
@@ -3,15 +3,11 @@ package com.example.wwmeet_backend.controller;
 import com.example.wwmeet_backend.domain.Appointment;
 import com.example.wwmeet_backend.domain.Participant;
 import com.example.wwmeet_backend.dto.AppointmentRequestDto;
-import com.example.wwmeet_backend.dto.AppointmentResponseDto;
-import com.example.wwmeet_backend.mapper.AppointmentMapper;
 import com.example.wwmeet_backend.mapper.AppointmentMapperImpl;
 import com.example.wwmeet_backend.service.AppointmentService;
 import com.example.wwmeet_backend.service.ParticipantService;
-import com.example.wwmeet_backend.service.VoteService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -23,9 +19,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.BDDMockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AppointmentController.class)
 class AppointmentControllerTest {
@@ -41,17 +39,23 @@ class AppointmentControllerTest {
     void findAppointmentAllListTest() throws Exception{
         List<Appointment> testList = new ArrayList<>();
         testList.add(new Appointment(1L, "test appointment", "두정", "test1", 3, null));
+        testList.add(new Appointment(2L, "test appointment", "두정", "test2", 2, null));
 
+
+        Participant participant = new Participant(1L, testList.get(0), "me");
         given(appointmentService.findAllAppointment(anyList()))
                 .willReturn(testList);
+        given(participantService.addParticipantOfAppointment(any()))
+                .willReturn(participant);
 
         mvc.perform(
                 get("/api/appointments")
-                    .param("appointmentCodeList", "test1")
+                    .param("appointmentCodeList", "testList")
+                    .param("participantName", "me")
                 )
                 .andExpect(status().isOk())
                 .andDo(print())
-                .andExpect(jsonPath("$..appointmentCode", "test1").exists());
+                .andExpect(jsonPath("$..identificationCode", "test1").exists());
     }
 
     @Test
@@ -85,7 +89,7 @@ class AppointmentControllerTest {
         String appointmentJson = mapper.writeValueAsString(requestAppointment);
 
         mvc.perform(post("/api/appointment")
-                        .content(appointmentJson + " " + participantNameJson)
+                        .content(appointmentJson + " " + participantNameJson) // 입력 json 다시
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(print())
