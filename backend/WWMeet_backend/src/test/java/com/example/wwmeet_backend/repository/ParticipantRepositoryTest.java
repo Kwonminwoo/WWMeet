66548b2a63e0378e90b7c53ca17d0ead561c6bb2
KwonMinwoo <qwerty029369@naver.com>
2023-09-16 12:27:00 +0900

    #36 - 약속 참가자 추가 Repository 테스트 코드
    약속의 참가자를 추가하는 기능의 Repository 테스트 코드를 작성함

---
+++ backend/WWMeet_backend/src/test/java/com/example/wwmeet_backend/repository/ParticipantRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.example.wwmeet_backend.repository;
+
+import com.example.wwmeet_backend.domain.Appointment;
+import com.example.wwmeet_backend.domain.Participant;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@TestPropertySource("classpath:application-test.properties")
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2, replace = AutoConfigureTestDatabase.Replace.ANY)
+class ParticipantRepositoryTest {
+    @Autowired
+    ParticipantRepository participantRepository;
+    @Test
+    void saveParticipant(){
+        Participant participant = new Participant(null, new Appointment(1L, "test", "test", "test", 2, null), "me");
+
+        Participant savedParticipant = participantRepository.save(participant);
+
+        Assertions.assertThat(participant.getParticipantName()).isEqualTo(savedParticipant.getParticipantName());
+    }
+}
