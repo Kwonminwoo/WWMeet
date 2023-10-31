a856734da266cc5451a1a41847474ff0d3193b9a
KwonMinwoo <qwerty029369@naver.com>
2023-10-31 23:38:09 +0900

    Refactor: vote, possible schedule 도메인 수정
    도메인 생성자 리팩토링

--- backend/WWMeet_backend/src/main/java/com/example/wwmeet_backend/possibleschedule/domain/PossibleSchedule.java
+++ backend/WWMeet_backend/src/main/java/com/example/wwmeet_backend/possibleschedule/domain/PossibleSchedule.java
@@ -1,28 +1,50 @@
 package com.example.wwmeet_backend.possibleschedule.domain;
 
 import com.example.wwmeet_backend.appointment.domain.Appointment;
-import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 
 @Entity
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Table(indexes = {
-        @Index(columnList = "id")
-})
 public class PossibleSchedule {
     @Id
     @GeneratedValue
     private Long id;
 
-    @Setter
     @ManyToOne
     @JoinColumn(name = "appointment_id")
     private Appointment appointment;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
+
+    protected PossibleSchedule() {
+    }
+
+    private PossibleSchedule(Long id, Appointment appointment, LocalDateTime startTime, LocalDateTime endTime) {
+        this.id = id;
+        this.appointment = appointment;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public static PossibleSchedule of(Long id, Appointment appointment, LocalDateTime startTime, LocalDateTime endTime) {
+        return new PossibleSchedule(id, appointment, startTime, endTime);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Appointment getAppointment() {
+        return appointment;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
 }
