413f23d085b6a30da9c623a400bfe8b594a91206
KwonMinwoo <qwerty029369@naver.com>
2023-10-31 23:38:35 +0900

    Refactor: save vote 기능 수정
    투표 기능 수정

--- backend/WWMeet_backend/src/main/java/com/example/wwmeet_backend/vote/service/VoteService.java
+++ backend/WWMeet_backend/src/main/java/com/example/wwmeet_backend/vote/service/VoteService.java
@@ -1,20 +1,47 @@
 package com.example.wwmeet_backend.vote.service;
 
 
+import com.example.wwmeet_backend.appointment.repository.AppointmentRepository;
+import com.example.wwmeet_backend.participant.domain.Participant;
+import com.example.wwmeet_backend.participant.repository.ParticipantRepository;
+import com.example.wwmeet_backend.possibleschedule.domain.PossibleSchedule;
+import com.example.wwmeet_backend.possibleschedule.dto.SavePossibleScheduleRequest;
+import com.example.wwmeet_backend.possibleschedule.repository.PossibleScheduleRepository;
 import com.example.wwmeet_backend.vote.domain.Vote;
+import com.example.wwmeet_backend.vote.dto.SaveVoteRequest;
 import com.example.wwmeet_backend.vote.repository.VoteRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
 @Slf4j
 public class VoteService {
     private final VoteRepository voteRepository;
-    public Vote saveVoteSchedule(Vote vote) {
-        return voteRepository.save(vote);
+    private final ParticipantRepository participantRepository;
+    private final PossibleScheduleRepository possibleScheduleRepository;
+    public Long saveVoteSchedule(SaveVoteRequest saveVoteRequest) {
+        Participant foundParticipant = participantRepository.findByParticipantName(saveVoteRequest.getParticipantName());
+
+        List<PossibleSchedule> possibleScheduleList = new ArrayList<>();
+        for (SavePossibleScheduleRequest possibleScheduleRequest : saveVoteRequest.getPossibleScheduleList()) {
+            PossibleSchedule savedPossibleSchedule = possibleScheduleRepository.save(
+                    PossibleSchedule.of(null, foundParticipant.getAppointment(), possibleScheduleRequest.getStartTime(), possibleScheduleRequest.getEndTime())
+            );
+            possibleScheduleList.add(savedPossibleSchedule);
+        }
+
+        possibleScheduleList.stream()
+                .map(possibleSchedule -> (Vote.of(null, foundParticipant, possibleSchedule)))
+                .forEach(voteRepository::save);
+
+        return foundParticipant.getAppointment().getId();
     }
 }
