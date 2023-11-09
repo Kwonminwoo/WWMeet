e80400eb5deeb8bbe1cae82adf8352feee61e0a1
gisu <hks3847@naver.com>
2023-11-10 01:24:56 +0900

    Feat: activity_appointment_create 레이아웃 작성
    appointment-create-page 화면, 기능 구성

---
+++ frontend/app/src/main/java/com/example/wwmeet_android/AppointmentCreateActivity.java
@@ -0,0 +1,50 @@
+package com.example.wwmeet_android;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AppointmentCreateActivity extends AppCompatActivity {
+
+    EditText nameEdit,usernameEdit,placeEdit;
+    Button createBtn;
+    ImageButton addBtn, minusBtn;
+    TextView numText;
+    int count = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment_create);
+        init();
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                numText.setText(count+"");
+            }
+        });
+        minusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count--;
+                numText.setText(count+"");
+            }
+        });
+    }
+    private void init(){
+        nameEdit = findViewById(R.id.appointment_create_name_edit);
+        usernameEdit = findViewById(R.id.appointment_create_user_name_edit);
+        placeEdit = findViewById(R.id.appointment_create_place_edit);
+        createBtn = findViewById(R.id.appointment_create_btn);
+        addBtn = findViewById(R.id.appointment_create_add_btn);
+        minusBtn = findViewById(R.id.appointment_create_minus_btn);
+        numText = findViewById(R.id.appointment_create_num_people_text);
+        numText.setText(count+"");
+    }
+}
+
