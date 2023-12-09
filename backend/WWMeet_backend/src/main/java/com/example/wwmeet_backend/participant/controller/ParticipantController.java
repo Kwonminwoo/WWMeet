75b120c54a20664dc79b13d62688244a03349fd5
kwonMInwoo <qwerty029369@naver.com>
2023-12-09 16:20:34 +0900

    feat: 약속의 모든 참가자 조회 기능
    특정 약속의 모든 참가자를 조회할 수 있는 기능 추가

--- backend/WWMeet_backend/src/main/java/com/example/wwmeet_backend/participant/controller/ParticipantController.java
+++ backend/WWMeet_backend/src/main/java/com/example/wwmeet_backend/participant/controller/ParticipantController.java
@@ -3,8 +3,12 @@ package com.example.wwmeet_backend.participant.controller;
 
 import com.example.wwmeet_backend.appointment.service.AppointmentService;
 import com.example.wwmeet_backend.participant.dto.AddParticipantRequest;
+import com.example.wwmeet_backend.participant.dto.FindParticipantResponse;
 import com.example.wwmeet_backend.participant.service.ParticipantService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +21,10 @@ public class ParticipantController {
     @PostMapping("/api/appointments/participant")
     public Long saveParticipantOfAppointment(@RequestBody AddParticipantRequest addParticipantRequest){
         return participantService.addParticipantByIdentificationCode(addParticipantRequest);
+    }
+
+    @GetMapping("/api/participants/{appointment_id}")
+    public List<FindParticipantResponse> findAllParticipantOfAppointment(@PathVariable("appointment_id") Long id){
+        return participantService.getParticipantList(id);
     }
 }
--- backend/WWMeet_backend/src/main/java/com/example/wwmeet_backend/participant/service/ParticipantService.java
+++ backend/WWMeet_backend/src/main/java/com/example/wwmeet_backend/participant/service/ParticipantService.java
@@ -4,7 +4,10 @@ import com.example.wwmeet_backend.appointment.domain.Appointment;
 import com.example.wwmeet_backend.appointment.repository.AppointmentRepository;
 import com.example.wwmeet_backend.participant.domain.Participant;
 import com.example.wwmeet_backend.participant.dto.AddParticipantRequest;
+import com.example.wwmeet_backend.participant.dto.FindParticipantResponse;
 import com.example.wwmeet_backend.participant.repository.ParticipantRepository;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,6 +30,17 @@ public class ParticipantService {
     public void addParticipantByAppointmentId(String participantName, Long id){
         Participant participant = Participant.of(appointmentRepository.findById(id).orElseThrow(NoSuchElementException::new), participantName);
         participantRepository.save(participant);
+    }
+
+    public List<FindParticipantResponse> getParticipantList(Long id){
+        List<Participant> foundParticipantList = participantRepository.findByAppointmentId(id);
+        List<FindParticipantResponse> responseList = new ArrayList<>();
+        for (Participant participant : foundParticipantList) {
+            FindParticipantResponse response = new FindParticipantResponse(participant.getParticipantName(), participant.getVoteList().size() != 0 ? true : false);
+            responseList.add(response);
+        }
+
+        return responseList;
     }
 
 }
