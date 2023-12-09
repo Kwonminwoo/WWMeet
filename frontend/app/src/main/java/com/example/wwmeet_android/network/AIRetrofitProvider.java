3f6475995d92baa37f0a8700413b81152aec30d2
kwonMInwoo <qwerty029369@naver.com>
2023-12-09 22:21:06 +0900

    feat: 안드로이드 AI 연결을 위한 레트로핏 설정
    AI 서버와 연결을 위한 안드로이드 레트로핏 설정

---
+++ frontend/app/src/main/java/com/example/wwmeet_android/network/AIRetrofitProvider.java
@@ -0,0 +1,29 @@
+package com.example.wwmeet_android.network;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class AIRetrofitProvider {
+    private Gson gson;
+    private RetrofitAIService retrofitAIService;
+
+    private final String BASE_URL = "http://10.0.2.2:5000";
+
+    public AIRetrofitProvider() {
+        gson = new GsonBuilder().create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        retrofitAIService = retrofit.create(RetrofitAIService.class);
+    }
+
+    public RetrofitAIService getService(){
+        return retrofitAIService;
+    }
+
+}
