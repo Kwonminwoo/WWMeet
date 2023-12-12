da226add58248e17bc54f40496905d07a44d723e
kwonMInwoo <qwerty029369@naver.com>
2023-12-12 13:21:19 +0900

    feat: 커스텀 타임피커 생성
    시간만 조정할 수 있는 커스텀 타임피커 생성 후 적용완료

--- frontend/app/src/main/java/com/example/wwmeet_android/appointment/create/SetVoteDeadlineActivity.java
+++ frontend/app/src/main/java/com/example/wwmeet_android/appointment/create/SetVoteDeadlineActivity.java
@@ -1,16 +1,24 @@
 package com.example.wwmeet_android.appointment.create;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.ColorSpace;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.wwmeet_android.appointment.vote.VoteScheduleActivity;
 import com.example.wwmeet_android.util.LocalDatabaseUtil;
 import com.example.wwmeet_android.R;
 import com.example.wwmeet_android.appointment.info.AppointmentInfoBeforeActivity;
@@ -28,14 +36,62 @@ import retrofit2.Response;
 
 public class SetVoteDeadlineActivity extends AppCompatActivity {
     DatePicker datePicker;
-    TimePicker timePicker;
     Button confirmBtn;
     private RetrofitService retrofitService;
+    private FrameLayout timePickerBox;
+    private TextView timeText;
+    private TextView afternoonText;
+    private TextView morningText;
+    private int nowTime = 1;
+    private boolean isAfternoon = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_vote_deadline);
+
+        timePickerBox = findViewById(R.id.vote_deadline_time_picker_box);
+        LayoutInflater layoutInflater = getLayoutInflater();
+        layoutInflater.inflate(R.layout.custom_time_picker, timePickerBox, true);
+
         init();
+        timePickerBox.findViewById(R.id.time_picker_plus).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(nowTime == 12){
+                    nowTime = 0;
+                }
+                timeText.setText(String.valueOf(++nowTime));
+            }
+        });
+
+
+        timePickerBox.findViewById(R.id.time_picker_minus).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(nowTime == 1){
+                    return;
+                }
+                timeText.setText(String.valueOf(--nowTime));
+            }
+        });
+
+        afternoonText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                afternoonText.setTextColor(Color.BLACK);
+                morningText.setTextColor(Color.GRAY);
+                isAfternoon = !isAfternoon;
+            }
+        });
+
+        morningText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                afternoonText.setTextColor(Color.GRAY);
+                morningText.setTextColor(Color.BLACK);
+                isAfternoon = !isAfternoon;
+            }
+        });
         confirmBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,16 +138,23 @@ public class SetVoteDeadlineActivity extends AppCompatActivity {
         int year = datePicker.getYear();
         int month = datePicker.getMonth();
         int day = datePicker.getDayOfMonth();
-        int hour = timePicker.getHour();
-        int minute = timePicker.getMinute();
 
-
-        return LocalDateTime.of(year, month + 1, day, hour, minute).toString();
+        int hour = Integer.parseInt(timeText.getText().toString());
+        if(isAfternoon){
+            hour += 12;
+            if(hour == 24){
+                hour = 0;
+            }
+        }
+        return LocalDateTime.of(year, month + 1, day, hour, 0).toString();
     }
     private void init(){
         confirmBtn = findViewById(R.id.set_vote_deadline_create_btn);
         datePicker = findViewById(R.id.set_vote_deadline_datePicker);
-        timePicker = findViewById(R.id.set_vote_deadline_timepicker);
+
+        timeText = timePickerBox.findViewById(R.id.time_picker_time);
+        afternoonText = timePickerBox.findViewById(R.id.time_picker_afternoon);
+        morningText = timePickerBox.findViewById(R.id.time_picker_morning);
 
         RetrofitProvider retrofitProvider = new RetrofitProvider();
         retrofitService = retrofitProvider.getService();
