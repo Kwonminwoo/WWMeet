e6820c43ff9b34b783ad2b7b55cfcee8a6c6bd6c
kwonMInwoo <qwerty029369@naver.com>
2023-12-16 22:47:05 +0900

    feat: 동적 크롤링
    맛집의 이미지를 가져오기 위해 동적 크롤링을 하는 기능 추가

---
+++ backend/WWMeet_backend/src/main/java/com/example/wwmeet_backend/restaurant/RestaurantCrawlingService.java
@@ -0,0 +1,42 @@
+package com.example.wwmeet_backend.restaurant;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class RestaurantCrawlingService {
+    public List<String> getRestaurantImageList(List<String> urlList){
+        long start = System.currentTimeMillis();
+        List<String> imageUrlList = new ArrayList<>();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--disable--gpu");
+        chromeOptions.addArguments("--disable-popup-blocking");
+        System.setProperty("webdriver.chrome.driver", "/Users/qwert/Downloads/chromedriver-mac-arm64/chromedriver");
+        ChromeDriver chromeDriver = new ChromeDriver(chromeOptions);
+
+        for (String url : urlList) {
+            log.error(url);
+            chromeDriver.get(url);
+            chromeDriver.manage().timeouts().implicitlyWait(Duration.ofMillis(2000));
+
+            WebElement div = chromeDriver.findElement(By.className("bg_present"));
+            String[] imageUrlArray = div.getAttribute("style").split("\"");
+            imageUrlList.add("https:" + imageUrlArray[1]);
+        }
+
+        long end = System.currentTimeMillis();
+
+        log.error(end - start + " ms");
+        return imageUrlList;
+    }
+}
