7aac0238b3719e9838ae076e1493d1df3cfc3fec
kwonMInwoo <qwerty029369@naver.com>
2023-12-16 22:49:21 +0900

    feat: 맛집 찾기 기능 및 무한 스크롤 구현
    맛집 찾기기능 완성, 무한 스크롤 구현 중 오류 발생

--- frontend/app/src/main/java/com/example/wwmeet_android/appointment/info/FoodSearchActivity.java
+++ frontend/app/src/main/java/com/example/wwmeet_android/appointment/info/FoodSearchActivity.java
@@ -1,11 +1,22 @@
 package com.example.wwmeet_android.appointment.info;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.LinearGradient;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,10 +26,14 @@ import com.example.wwmeet_android.dto.kakao.KakoSearchResponse;
 import com.example.wwmeet_android.dto.kakao.SearchRestaurantResponse;
 import com.example.wwmeet_android.network.KakaoApiRetrofitProvider;
 import com.example.wwmeet_android.network.KakaoService;
+import com.example.wwmeet_android.network.RetrofitProvider;
+import com.example.wwmeet_android.network.RetrofitService;
 import com.example.wwmeet_android.util.KaKaoAPI;
+import com.facebook.shimmer.ShimmerFrameLayout;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import retrofit2.Call;
@@ -29,71 +44,179 @@ public class FoodSearchActivity extends AppCompatActivity {
     private RecyclerView foodRecyclerView;
     private TextView placeNameText;
     private KakaoService kakaoService;
+    private RetrofitService retrofitService;
     private String address;
     private String foodName;
     private List<Restaurant> restaurantList = new ArrayList<>();
+    private List<Restaurant> restaurantListFromServer = new ArrayList<>();
+    private List<String> restaurantUrlList = new ArrayList<>();
     private RestaurantListAdapter restaurantListAdapter;
 
+    private ShimmerFrameLayout skeletonFrame;
+    private LinearLayout restaurantListBox;
+    private Thread apiThread;
+
+    private boolean isLoading = false;
+    private int page = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_search);
-
         init();
+        
+        foodRecyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
 
-        searchRestaurantFromKakao();
+                if(!isLoading){
+                    if (!foodRecyclerView.canScrollVertically(1)) {
+                        Toast.makeText(FoodSearchActivity.this, "last", Toast.LENGTH_SHORT).show();
+                        isLoading = true;
+                        loadMoreData();
+                    }
+                }
+            }
+        });
 
+        getRestaurantsData();
     }
 
     private void init(){
         foodRecyclerView = findViewById(R.id.food_recyclerview);
         placeNameText = findViewById(R.id.food_place);
         restaurantListAdapter = new RestaurantListAdapter();
+        skeletonFrame = findViewById(R.id.skeleton_restaurant);
+        restaurantListBox = findViewById(R.id.restart_search_list);
 
         KakaoApiRetrofitProvider kakaoApiRetrofitProvider = new KakaoApiRetrofitProvider();
         kakaoService = kakaoApiRetrofitProvider.getService();
+
+        RetrofitProvider retrofitProvider = new RetrofitProvider();
+        retrofitService = retrofitProvider.getService();
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        foodRecyclerView.setLayoutManager(linearLayoutManager);
+        foodRecyclerView.setAdapter(restaurantListAdapter);
     }
 
-    private void searchRestaurantFromKakao(){
-        String categoryCode = "FD6";
-        Call<KakoSearchResponse> searchRestaurantCall = kakaoService.searchRestaurant(KaKaoAPI.KAKAO_AK, "강남역 카레");
-        searchRestaurantCall.enqueue(new Callback<KakoSearchResponse>() {
+    private void getRestaurantsData(){
+        apiThread = new Thread(new Runnable() {
             @Override
-            public void onResponse(Call<KakoSearchResponse> call, Response<KakoSearchResponse> response) {
+            public void run() {
+                Call<KakoSearchResponse> searchRestaurantCall = kakaoService.searchRestaurant(KaKaoAPI.KAKAO_AK, "강남역 카레", 5, page++);
+                try {
+                    Response<KakoSearchResponse> response = searchRestaurantCall.execute();
+                    if (!response.isSuccessful()) {
+                        Toast.makeText(FoodSearchActivity.this, "검색에 실패했습니다.", Toast.LENGTH_SHORT).show();
+                        try {
+                            Log.e("검색 실패", response.errorBody().string());
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                        return;
+                    }
+
+                    KakoSearchResponse searchResponse = response.body();
+                    List<SearchRestaurantResponse> documents = searchResponse.getDocuments();
+                    List<Restaurant> resultList = new ArrayList<>();
+                    restaurantUrlList.clear();
+                    for (SearchRestaurantResponse searchRestaurant : documents) {
+                        Restaurant restaurant = new Restaurant();
+                        restaurant.setRestaurantName(searchRestaurant.getPlaceName());
+                        restaurant.setAddress(searchRestaurant.getRoadAddressName());
+                        restaurant.setPhone(searchRestaurant.getPhone());
+                        String[] categoryArray = searchRestaurant.getCategoryName().split(" > ");
+                        restaurant.setMenu(categoryArray[categoryArray.length - 1]);
+
+                        restaurantUrlList.add(searchRestaurant.getPlaceUrl().replace("http", "https"));
+
+                        resultList.add(restaurant);
+                    }
+
+                    restaurantListFromServer = resultList;
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        apiThread.start();
+
+        try {
+            apiThread.join();
+            getCrawlingRestaurantImage(restaurantUrlList);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void getCrawlingRestaurantImage(List<String> urlList) {
+        Log.e("t", "image");
+
+        Call<List<String>> getImageUrlListCall = retrofitService.getRestaurantImageList(urlList);
+        getImageUrlListCall.enqueue(new Callback<List<String>>() {
+            @Override
+            public void onResponse(Call<List<String>> call, Response<List<String>> response) {
                 if (!response.isSuccessful()) {
-                    Toast.makeText(FoodSearchActivity.this, "검색에 실패했습니다.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(FoodSearchActivity.this, "이미지 조회에 실패했습니다.", Toast.LENGTH_SHORT).show();
                     try {
-                        Log.e("검색 실패", response.errorBody().string());
+                        Log.e("이미지 조회 실패", response.errorBody().string());
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
                     return;
                 }
 
-                KakoSearchResponse searchResponse = response.body();
-                List<SearchRestaurantResponse> documents = searchResponse.getDocuments();
-                for(SearchRestaurantResponse res: documents){
-                    Log.e("url", res.getPlaceUrl());
+                for (int i = 0;i < restaurantListFromServer.size();i++) {
+                    Restaurant restaurant = restaurantListFromServer.get(i);
+                    List<String> imageUrlList = response.body();
+                    restaurant.setImageUrl(imageUrlList.get(i));
                 }
 
+                setRestaurantList();
+                showList();
+                restaurantListAdapter.notifyDataSetChanged();
+                isLoading = false;
             }
 
             @Override
-            public void onFailure(Call<KakoSearchResponse> call, Throwable t) {
+            public void onFailure(Call<List<String>> call, Throwable t) {
                 Toast.makeText(FoodSearchActivity.this, "서버 연결에 실패했습니다.", Toast.LENGTH_SHORT).show();
-                Log.e("서버 연결 실패", t.getMessage());
+                Log.e("서버 연결에 실패", t.getMessage());
             }
         });
     }
 
-    private void crawlingRestaurants(List<SearchRestaurantResponse> searchResultList){
-
+    private void setRestaurantList() {
+        for (Restaurant restaurant : restaurantListFromServer) {
+            restaurantListAdapter.addRestaurant(restaurant);
+        }
     }
 
-    private void setRestaurantList() {
-        restaurantListAdapter.setRestaurantList(restaurantList);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
-        foodRecyclerView.setLayoutManager(linearLayoutManager);
-        foodRecyclerView.setAdapter(restaurantListAdapter);
+    private void showList(){
+        skeletonFrame.setVisibility(View.GONE);
+        restaurantListBox.setVisibility(View.VISIBLE);
+    }
+
+    private void loadMoreData(){
+        restaurantListAdapter.addRestaurant(null);
+        restaurantListFromServer.add(null);
+
+        restaurantListAdapter.notifyDataSetChanged();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                restaurantListAdapter.notifyItemInserted(restaurantList.size() - 1);
+                int scrollPosition = restaurantListFromServer.size();
+
+                restaurantListFromServer.remove(scrollPosition - 1);
+//                restaurantListAdapter.removeRestaurant(scrollPosition - 1);
+
+                getRestaurantsData();
+            }
+        }, 2000);
     }
 }
--- frontend/app/src/main/java/com/example/wwmeet_android/appointment/info/RestaurantListAdapter.java
+++ frontend/app/src/main/java/com/example/wwmeet_android/appointment/info/RestaurantListAdapter.java
@@ -1,15 +1,18 @@
 package com.example.wwmeet_android.appointment.info;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.wwmeet_android.R;
 import com.example.wwmeet_android.domain.Restaurant;
 
@@ -17,16 +20,19 @@ import com.example.wwmeet_android.domain.Restaurant;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAdapter.ViewHolder> {
+public class RestaurantListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private final int VIEW_TYPE_ITEM = 0;
+    private final int VIEW_TYPE_LOADING = 1;
+
     private List<Restaurant> restaurantList = new ArrayList<>();
     private Context context;
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    private class ItemViewHolder extends RecyclerView.ViewHolder {
         private TextView restaurantNameText;
         private TextView menuText;
         private TextView addressText;
         private ImageView imageView;
-        public ViewHolder(@NonNull View itemView) {
+        public ItemViewHolder(@NonNull View itemView) {
             super(itemView);
             restaurantNameText = itemView.findViewById(R.id.item_restaurant_name);
             menuText = itemView.findViewById(R.id.item_restaurant_menu);
@@ -35,28 +41,55 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
         }
     }
 
-    public void setRestaurantList(List<Restaurant> list){
-        this.restaurantList = list;
+    public class LoadingViewHolder extends RecyclerView.ViewHolder {
+        private ProgressBar progressBar;
+
+        public LoadingViewHolder(@NonNull View itemView) {
+            super(itemView);
+            progressBar = itemView.findViewById(R.id.item_progressbar);
+        }
+    }
+
+    public void addRestaurant(Restaurant restaurant){
+        restaurantList.add(restaurant);
+    }
+
+    public void removeRestaurant(int position){
+        restaurantList.remove(position);
     }
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_restaurant_list, parent, false);
-        RestaurantListAdapter.ViewHolder viewHolder = new RestaurantListAdapter.ViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         context = parent.getContext();
-
-        return viewHolder;
+        if(viewType == VIEW_TYPE_ITEM){
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_restaurant_list, parent, false);
+            return new ItemViewHolder(view);
+        }else{
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_progressbar, parent, false);
+            return new LoadingViewHolder(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Restaurant restaurant = restaurantList.get(position);
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if(holder instanceof ItemViewHolder){
+            Restaurant restaurant = restaurantList.get(position);
+            ((ItemViewHolder) holder).restaurantNameText.setText(restaurant.getRestaurantName());
+            ((ItemViewHolder) holder).menuText.setText(restaurant.getMenu());
+            ((ItemViewHolder) holder).addressText.setText(restaurant.getAddress());
+            Glide.with(context)
+                    .load(restaurant.getImageUrl())
+                    .into(((ItemViewHolder) holder).imageView);
+            return;
+        } else if (holder instanceof LoadingViewHolder) {
+            Log.e("e", "loading");
+        }
+    }
 
-        holder.restaurantNameText.setText(restaurant.getRestaurantName());
-        holder.menuText.setText(restaurant.getMenu());
-        holder.addressText.setText(restaurant.getAddress());
-        // 이미지 어떻게 가져와지는지 확인 후
+    @Override
+    public int getItemViewType(int position) {
+        return restaurantList.get(position) == null ? VIEW_TYPE_LOADING : VIEW_TYPE_ITEM;
     }
 
     @Override
