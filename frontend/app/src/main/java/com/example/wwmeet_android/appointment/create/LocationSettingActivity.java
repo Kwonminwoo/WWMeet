930cdbc96092c3cc11104bb2b3241549f6a4db7a
kwonMInwoo <qwerty029369@naver.com>
2023-12-19 01:51:40 +0900

    feat: 네이버 맵 기능 추가
    네이버 맵의 자신의 위치 추가 마커 찍기 까지 완료

--- frontend/app/src/main/java/com/example/wwmeet_android/appointment/create/LocationSettingActivity.java
+++ frontend/app/src/main/java/com/example/wwmeet_android/appointment/create/LocationSettingActivity.java
@@ -1,7 +1,10 @@
 package com.example.wwmeet_android.appointment.create;
 
+import android.Manifest;
 import android.content.Intent;
+import android.graphics.PointF;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,18 +12,40 @@ import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.wwmeet_android.R;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.util.FusedLocationSource;
 
-public class LocationSettingActivity extends AppCompatActivity {
+public class LocationSettingActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     EditText placeEdit;
     ImageView searchBtn;
 
-    FrameLayout mapFrame;
+    MapView mapFrame;
     Button createBtn;
+
+    private static final String[] PERMISSION = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+
+    private Marker marker = new Marker();
+
+    private NaverMap naverMap;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private FusedLocationSource locationSource;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,12 +59,49 @@ public class LocationSettingActivity extends AppCompatActivity {
                 startActivity(intent); */
             }
         });
+
+
+        mapFrame.onCreate(savedInstanceState);
+        mapFrame.getMapAsync(this);
+        locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
+
+
+
     }
     private void init(){
         placeEdit = findViewById(R.id.location_setting_search_edit);
         searchBtn = findViewById(R.id.location_setting_search_btn);
-        mapFrame = findViewById(R.id.location_setting_kakao_map);
+        mapFrame = findViewById(R.id.location_setting_map_view);
         createBtn = findViewById(R.id.location_setting_create_btn);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated()) { // 권한 거부됨
+
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap = naverMap;
+        naverMap.setLocationSource(locationSource);
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+        // 권한 확인, 결과는 onRequestPermissionResult 콜백 메서드 호출
+        ActivityCompat.requestPermissions(this, PERMISSION, LOCATION_PERMISSION_REQUEST_CODE);
+
+        naverMap.setOnMapClickListener(new NaverMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull PointF pointF, @NonNull LatLng latLng) {
+                marker.setPosition(latLng);
+
+                marker.setMap(naverMap);
+            }
+        });
     }
 }
 
