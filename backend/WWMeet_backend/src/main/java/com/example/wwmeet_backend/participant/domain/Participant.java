54376f9b518de94570b1eff8af80655645bced9d
kwonMInwoo <qwerty029369@naver.com>
2024-03-05 13:08:27 +0900

    feat: member 도메인 추가
    member엔티티 추가

--- backend/WWMeet_backend/src/main/java/com/example/wwmeet_backend/participant/domain/Participant.java
+++ backend/WWMeet_backend/src/main/java/com/example/wwmeet_backend/participant/domain/Participant.java
@@ -1,13 +1,19 @@
 package com.example.wwmeet_backend.participant.domain;
 
 import com.example.wwmeet_backend.appointment.domain.Appointment;
+import com.example.wwmeet_backend.member.domain.Member;
 import com.example.wwmeet_backend.vote.domain.Vote;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.*;
-
-import jakarta.persistence.*;
-import org.springframework.boot.SpringApplication;
+import lombok.Getter;
 
 @Entity
 @Getter
@@ -20,6 +26,10 @@ public class Participant {
     private Appointment appointment;
 
     private String participantName;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
 
     @OneToMany(mappedBy = "participant", fetch = FetchType.LAZY)
     private List<Vote> voteList = new ArrayList<>();
