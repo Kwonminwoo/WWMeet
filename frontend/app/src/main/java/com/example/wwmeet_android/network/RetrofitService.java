513c3f9821f0a85e4c3912da396055b0de96608a
kwonMInwoo <qwerty029369@naver.com>
2024-03-14 13:15:55 +0900

    feat: 토큰 저장, 사용 로직 추가
    토큰 저장하고 사용하는 로직 추가
    Login시 토큰 반환값 받도록 수정

--- frontend/app/src/main/java/com/example/wwmeet_android/network/RetrofitService.java
+++ frontend/app/src/main/java/com/example/wwmeet_android/network/RetrofitService.java
@@ -17,6 +17,7 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
@@ -59,7 +60,7 @@ public interface RetrofitService {
     Call<List<String>> getRestaurantImageList(@Query("urlList") List<String> urlList);
 
     @POST("/api/members/signin")
-    Call<Long> signIn(@Body SignInRequest signInRequest);
+    Call<String> signIn(@Body SignInRequest signInRequest);
 
     @POST("/api/members/signup")
     Call<Void> signUp(@Body SignUpRequest signUpRequest);
