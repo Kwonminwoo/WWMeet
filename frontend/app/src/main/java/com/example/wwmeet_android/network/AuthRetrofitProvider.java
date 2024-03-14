82baa037120015c82d534627416b058f28b0d993
kwonMInwoo <qwerty029369@naver.com>
2024-03-14 13:12:37 +0900

    feat: 토큰, 설정 추가
    토큰 적용 및 설정 추가

---
+++ frontend/app/src/main/java/com/example/wwmeet_android/network/AuthRetrofitProvider.java
@@ -0,0 +1,26 @@
+package com.example.wwmeet_android.network;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class AuthRetrofitProvider extends RetrofitProvider{
+
+    public AuthRetrofitProvider(String token) {
+        OkHttpClient.Builder client = new OkHttpClient.Builder();
+        client.addInterceptor(new TokenInterceptor(token));
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(client.build())
+                .build();
+
+        retrofitService = retrofit.create(RetrofitService.class);
+    }
+
+    public RetrofitService getService(){
+        return retrofitService;
+    }
+
+}
--- frontend/app/src/main/java/com/example/wwmeet_android/network/RetrofitProvider.java
+++ frontend/app/src/main/java/com/example/wwmeet_android/network/RetrofitProvider.java
@@ -1,30 +1,41 @@
 package com.example.wwmeet_android.network;
 
+import androidx.annotation.NonNull;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class RetrofitProvider {
-    private Gson gson;
-    private RetrofitService retrofitService;
+    protected Gson gson;
+    protected RetrofitService retrofitService;
 
-    private final String BASE_URL = "http://10.0.2.2:8080";
-//    private final String BASE_URL = "http://10.0.2.1:8080";
-//    private final String BASE_URL = "http://10.2.19.84:8080";
+    protected final String BASE_URL = "http://10.0.2.2:8080";
+//    protected final String BASE_URL = "http://10.0.2.1:8080";
+//    protected final String BASE_URL = "http://10.2.19.84:8080";
 
     public RetrofitProvider() {
-        gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+        gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").setLenient().create();
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .addConverterFactory(ScalarsConverterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build();
+
         retrofitService = retrofit.create(RetrofitService.class);
     }
 
     public RetrofitService getService(){
         return retrofitService;
     }
+
 }
