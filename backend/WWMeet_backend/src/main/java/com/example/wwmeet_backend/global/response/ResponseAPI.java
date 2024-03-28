845f9b0d281e01b12cf49ec60cef29dc1211109d
kwonMInwoo <qwerty029369@naver.com>
2024-03-28 15:41:49 +0900

    feat: base Response객체 생성
    Api반환 시 사용할 Response 객체 생성

---
+++ backend/WWMeet_backend/src/main/java/com/example/wwmeet_backend/global/response/ResponseAPI.java
@@ -0,0 +1,27 @@
+package com.example.wwmeet_backend.global.response;
+
+public class ResponseAPI {
+    private String message;
+    private int code;
+    private Object data;
+
+    public ResponseAPI(String message, int code, Object data) {
+        this.message = message;
+        this.code = code;
+        this.data = data;
+    }
+
+    public ResponseAPI(String message, Object data) {
+        this.message = message;
+        this.data = data;
+    }
+
+    public static ResponseAPI response(String message, int code, Object data) {
+        return new ResponseAPI(message, code, data);
+    }
+
+    public static ResponseAPI response(String message, Object data) {
+        return new ResponseAPI(message, data);
+    }
+
+}
