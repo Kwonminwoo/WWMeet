e799da30830f7cc2071e4d5d2c26d63edf0e8d47
kwonMInwoo <qwerty029369@naver.com>
2024-04-05 15:10:31 +0900

    feat: response에 getter추가
    역직렬화를 위한 getter 추가

--- backend/WWMeet_backend/src/main/java/com/example/wwmeet_backend/domain/member/dto/response/SignInResponse.java
+++ backend/WWMeet_backend/src/main/java/com/example/wwmeet_backend/domain/member/dto/response/SignInResponse.java
@@ -1,8 +1,10 @@
 package com.example.wwmeet_backend.domain.member.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class SignInResponse {
     private String accessToken;
     private String refreshToken;
